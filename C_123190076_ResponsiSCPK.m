d5cc3fd859d4e1e28feca84b488ca309a77c53c1
BayuFPamungkas <86466850+BayuFPamungkas@users.noreply.github.com>
2021-06-26 09:57:27 +0700

    Add files via upload

---
+++ C_123190076_ResponsiSCPK.m
@@ -0,0 +1,132 @@
+function varargout = C_123190076_ResponsiSCPK(varargin)
+% C_123190076_ResponsiSCPK MATLAB code for C_123190076_ResponsiSCPK.fig
+%      C_123190076_ResponsiSCPK, by itself, creates a new C_123190076_ResponsiSCPK or raises the existing
+%      singleton*.
+%
+%      H = C_123190076_ResponsiSCPK returns the handle to a new C_123190076_ResponsiSCPK or the handle to
+%      the existing singleton*.
+%
+%      C_123190076_ResponsiSCPK('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in C_123190076_ResponsiSCPK.M with the given input arguments.
+%
+%      C_123190076_ResponsiSCPK('Property','Value',...) creates a new C_123190076_ResponsiSCPK or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before C_123190076_ResponsiSCPK_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to C_123190076_ResponsiSCPK_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help C_123190076_ResponsiSCPK
+
+% Last Modified by GUIDE v2.5 25-Jun-2021 21:08:47
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @C_123190076_ResponsiSCPK_OpeningFcn, ...
+                   'gui_OutputFcn',  @C_123190076_ResponsiSCPK_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before C_123190076_ResponsiSCPK is made visible.
+function C_123190076_ResponsiSCPK_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to C_123190076_ResponsiSCPK (see VARARGIN)
+
+% Choose default command line output for C_123190076_ResponsiSCPK
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes C_123190076_ResponsiSCPK wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = C_123190076_ResponsiSCPK_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+data = xlsread('DATA RUMAH.xlsx','C2:H21');
+set(handles.uitable1,'data',data);
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+w = [0.3, 0.2, 0.23, 0.1, 0.07, 0.1];
+k = [0,1,1,1,1,1];
+x = xlsread('DATA RUMAH.xlsx','C2:H1011');
+
+[m,n]=size (x); 
+R=zeros (m,n); 
+%Y=zeros (m,n);
+for j=1:n
+    if k(j)==1
+        R(:,j)=x(:,j)./max(x(:,j));
+    else
+        R(:,j)=min(x(:,j))./x(:,j);
+    end
+end
+
+for i=1:m
+    V(i)= sum(w.*R(i,:));
+end
+
+rank = sort(V,'descend');
+
+for i=1:20
+    hasil(i) = rank(i);
+end
+
+
+opts2 = detectImportOptions('DATA RUMAH.xlsx'); %deteksi DATA RUMAH.xlsx
+opts2.SelectedVariableNames = [2]; %memilih hanya kolom Nama Rumah
+
+nama = readtable('DATA RUMAH.xlsx',opts2);
+
+for i=1:20
+ for j=1:m
+   if(hasil(i) == V(j))
+    seleksi(i) = nama(j);
+    break
+   end
+ end
+end
+
+seleksi = seleksi';
+
+set(handles.uitable2, 'data', seleksi);
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
